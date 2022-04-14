293dcc6f22d1161ffab6e962ed9dd80686a5cc7e
wissanu <thungrut@gmail.com>
2022-04-14 22:54:31 +0700

    custom module

---
+++ custom_module.lua
@@ -0,0 +1,12 @@
+_G.nmath = {}
+
+function nmath.add(x,y)
+    return x + y
+end
+
+function nmath.power(x,y)
+    return x ^ y
+end
+
+return nmath
+
---
+++ test_module.lua
@@ -0,0 +1,4 @@
+local mod = require("custom_module")
+
+print(mod.add(5, 10))
+print(mod.power(2,2))
